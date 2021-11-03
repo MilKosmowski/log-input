26311b79da53eeba5239265ebd676c549b7b89dc
MK <testteamsretset@gmail.com>
2021-11-03 13:28:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
