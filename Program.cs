d27ae37a31b5e51b12732c9d4ed6d3a50de75c45
MK <testteamsretset@gmail.com>
2021-11-03 15:30:28 +0100

    ReadLine & UserInput prop

--- Program.cs
+++ Program.cs
@@ -2,16 +2,24 @@
 
 namespace LogDataApp
 {
-    partial class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Which type of logging do You choose?: \n\n " +
+
                               "(C)onsole logging,\n" +
                               "(F)ile logging,\n" +
-                              "(A)ll of the above.\n");
-            Console.ReadLine();
+                              "(A)ll of the above?\n");
+            UserInput = Console.Read().ToString();
 
         }
+
+        public static char UserInput 
+        {
+            get {return userInput;}
+            set { if () userInput = value; }
+        }
+        private static char userInput; 
     }
 }
