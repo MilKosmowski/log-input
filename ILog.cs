e27eeca1de6620c958fe4aa1c2e0fecccf80fd60
kosmo <kosmo@DESKTOP-DMNVB3P>
2021-11-03 21:09:22 +0100

    Implement GenericSearch, UserInput filter.

--- ILog.cs
+++ ILog.cs
@@ -1,7 +1,46 @@
-﻿namespace LogDataApp
+﻿using System;
+
+namespace LogDataApp
 {
-    interface ILog
+    internal interface ILog
     {
-    
+        public void ExecuteLogging(string userInput);
+
     }
+
+    internal abstract class LogGeneric : ILog
+    {
+        public LogGeneric(string userInput) => this.userInput = userInput;
+        public abstract void ExecuteLogging(string userInput);
+
+        protected string userInput;
+    }
+    class LogToConsole : LogGeneric
+    {
+        LogToConsole(string userInput) : base(userInput) { }
+
+        public override void ExecuteLogging(string userInput)
+        {
+            System.Console.WriteLine($"{DateTime.Now,-10} - {userInput}");
+        }
+    }
+    class LogToFile : LogGeneric
+    {
+        LogToFile(string userInput) : base(userInput) { }
+
+        public override void ExecuteLogging(string userInput)
+        {
+            System.Console.WriteLine($"{DateTime.Now,-10} - {userInput}");
+        }
+    }
+    class LogToEventLog : LogGeneric
+    {
+        LogToEventLog(string userInput) : base(userInput) { }
+
+        public override void ExecuteLogging(string userInput)
+        {
+            System.Console.WriteLine($"{DateTime.Now,-10} - {userInput}");
+        }
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LogDataApp
 {
@@ -6,20 +7,31 @@ namespace LogDataApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Which type of logging do You choose?: \n\n " +
+            Console.WriteLine("-----------LOG USER INPUT------------\n\n" +
+                              "Which type of logging do You choose?: \n\n" +
 
                               "(C)onsole logging,\n" +
                               "(F)ile logging,\n" +
-                              "(A)ll of the above?\n");
-            UserInput = Console.Read().ToString();
+                              "(E)vent logging,\n" +
+                              "Default -> (A)ll of the above\n\n" +
 
+                              "Input a letter:  ");
+
+            UserInput = Char.ToUpper(Convert.ToChar(Console.Read()));
+
+            Console.WriteLine($"Option chosen: {userInput}. Input text to be logged:\n");
+
+            ILog logger = new LogToConsole(userInput);
+            Console.ReadLine();
         }
 
-        public static char UserInput 
+        public static char UserInput
         {
-            get {return userInput;}
-            set { if () userInput = value; }
+            get { return userInput; }
+            set { if (logOptions.Contains(value)) userInput = value;}
         }
-        private static char userInput; 
+
+        private static char userInput = 'A';
+        private static char[] logOptions = {'C','F','E','A'};
     }
 }
